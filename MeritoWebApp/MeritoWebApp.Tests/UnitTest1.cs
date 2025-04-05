d89fab92d5cfda5b552cf1f637da4ad41e09bad7
Wojcik94 <wojcik94@interia.pl>
2025-04-05 14:31:06 +0200

    Add test project and initial unit tests for WebApp functionality

---
+++ MeritoWebApp/MeritoWebApp.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace MeritoWebApp.Tests;
+
+public class WebAppTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
+
+public class HomeControllerTests
+{
+    [Fact]
+    public void testHelloWorld()
+    {
+        Assert.Equal("Hello World", "Hello World");
+    }
+}
